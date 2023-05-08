668df3c99c29040e7f027d95e7df1752ed5b9e05
shahrokh-t <tahmoores54@gmail.com>
2023-05-08 15:39:26 +1000

    set calorie limit

--- js/app.js
+++ js/app.js
@@ -58,6 +58,12 @@ class CalorieTracker {
         this._render();
     }
 
+    setLimit(calorieLimit) {
+        this._calorieLimit = calorieLimit;
+        this._displayCaloriesLimit();
+        this._render();
+    }
+
     // Private Methods
     _displayCaloriesTotal() {
         const totalCaloriesEl = document.getElementById("calories-total");
@@ -214,6 +220,8 @@ class App {
         document.getElementById("filter-workouts").addEventListener("keyup", this._filterItems.bind(this, "workout"));
 
         document.getElementById("reset").addEventListener("click", this._reset.bind(this));
+
+        document.getElementById("limit-form").addEventListener("submit", this._setLimit.bind(this));
     }
 
     _newItem(type, e) {
@@ -280,6 +288,24 @@ class App {
         document.getElementById("filter-meals").value = "";
         document.getElementById("filter-workouts").value = "";
 
+    }
+
+    _setLimit (e) {
+        e.preventDefault();
+
+        const limit = document.getElementById("limit");
+
+        if (limit.value === "") {
+            alert("Please add a limit");
+            return;
+        }
+
+        this._tracker.setLimit(+limit.value);
+        limit.value = "";
+
+        const modalEl = document.getElementById("limit-modal");
+        const modal = bootstrap.Modal.getInstance(modalEl);
+        modal.hide();
     }
 }
 
